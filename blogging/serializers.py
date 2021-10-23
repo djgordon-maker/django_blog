5611e8ac47f30ffcbd9d01aa211afd846ab9988a
djgordon-maker <dragongordon@gmail.com>
2021-10-23 08:42:19 -0700

    Create serializers.py

---
+++ blogging/serializers.py
@@ -0,0 +1,21 @@
+from blogging.models import User, Post, Catagory
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'author']
+
+
+class CatagorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Catagory
+        fields = ['name', 'description', 'posts']
+
