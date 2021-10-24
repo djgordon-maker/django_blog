5fca20be09567141c043888ce86b3fd1e6460ed7
djgordon-maker <dragongordon@gmail.com>
2021-10-23 19:00:49 -0700

    Added pagination

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = "/static/"
 
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
